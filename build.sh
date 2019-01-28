a26d1e3c1f94bf146b97aa38717bcde99dc7dccb
Daniel Gerlanc <dgerlanc@enplusadvisors.com>
2019-01-28 12:05:49 -0500

    Rerun scripts to get latest. Add notes in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 set -e
 
+# 1) Run all notebooks and check for errors
+# jupyter nbconvert --to notebook --execute *-slides.ipynb --inplace
+
 jupyter nbconvert --to slides --output-dir build *slides.ipynb
 
 # add converting to pdf
-# set query string to be ?print-pdf&pdfSeparateFragments=false
+# set query string to be ?print-pdf&pdfSeparateFragments=false&pdfMaxPagesPerSlide=1"
