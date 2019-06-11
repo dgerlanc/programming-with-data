7224acb2c3f07aacbc0c50a4a90a8e368946a312
Daniel Gerlanc <dgerlanc@enplusadvisors.com>
2019-06-10 21:05:22 -0400

    Update to output w/ proper extension.

--- build.py
+++ build.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+import logging
 import subprocess
 from pathlib import Path
 
+logger = logging.getLogger(__name__)
 
 # TODO: Only update files changed since last run
-# TODO: Add conversion tool from existing nbs
+# TODO: Add conversion tool from existing answer notebooks
 
-def answer2exercise(infile: Path, outfile: Path):
+def answer2exercise(infile, outfile):
     """
     Convert answer notebooks to exercise notebooks
     
@@ -19,39 +21,40 @@ def answer2exercise(infile: Path, outfile: Path):
     subprocess.run(cmd)
 
 
-def slide2html(infile: Path, outfile: Path):
+def slide2html(infile):
     """
     Convert slide notebooks to reveal.js
 
     """
-    cmd = ('jupyter nbconvert --to slides --reveal-prefix=reveal.js'
-           '--output-dir build --output').split()
-    cmd.extend([outfile, infile]
+    cmd = ('jupyter nbconvert'
+           ' --to slides'
+           ' --reveal-prefix=reveal.js'
+           ' --SlidesExporter.file_extension=.html'
+           ' --output-dir build').split()
+    cmd.append(infile)
     subprocess.run(cmd)
-               
+
     
 def main():
     p = Path('.')
 
     # TODO: execute slides and make sure no errors
+    
 
-               
-    # ipynb slides -> reveal.js html
+    logger.info('ipynb slides -> reveal.js html')
     slide_fns = p.glob('*slides.ipynb')
     for slide_fn in sorted(slide_fns):
-        html_slides = str(slide_fns).replace('ipynb', 'html')
-        # print(f'{answer_nb} -> {exercise_nb}')
-        slide2html(slide_nb, html_slides)
-               
-    # convert exercises to answers
+        slide2html(slide_fn)
+    
+    logger.info('Convert exercises to answers')
     answers = p.glob('*answers.ipynb')
     for answer_nb in sorted(answers):
         exercise_nb = str(answer_nb).replace('answer', 'exercise')
         # print(f'{answer_nb} -> {exercise_nb}')
         answer2exercise(answer_nb, exercise_nb)
 
-               
     # html slides -> pdf
+
 
                
 if __name__ == '__main__':
